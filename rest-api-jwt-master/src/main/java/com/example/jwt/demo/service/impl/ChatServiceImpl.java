743acdeeb76802bd5281d84467ab6dd2a55dd460
DESKTOP-1HJAFC2\thanh <thanhnt21092001@gmail.com>
2023-11-26 19:13:02 +0700

    commit

--- rest-api-jwt-master/src/main/java/com/example/jwt/demo/service/impl/ChatServiceImpl.java
+++ rest-api-jwt-master/src/main/java/com/example/jwt/demo/service/impl/ChatServiceImpl.java
@@ -21,7 +21,18 @@ public class ChatServiceImpl implements ChatService {
     public String getChatResponse(String message) {
         String response = chatSession.multisentenceRespond(message);
         // response = " BMI: height:168 age:22 ... "
-        History<String> history = chatSession.inputHistory;
+        if (message.matches("\\d+")) {
+            String[] parts = message.split(" ");
+            String part1 = parts[0];  // Phần đầu tiên
+            String part2 = parts[1];
+            int cc = Integer.parseInt(part1);
+            int canNang =Integer.parseInt(part2);
+            float ketQua = (float) cc /((float) canNang /100);
+            System.out.println();
+            History<String> history = chatSession.inputHistory;
+            return chatSession.multisentenceRespond(message);
+        }
         return chatSession.multisentenceRespond(message);
+
     }
 }
