32fc2da316418269d90c5c6efba48f0e515ca04c
loitran299 <loitran299@gmail.com>
2023-11-26 16:53:58 +0700

    logic bot

--- rest-api-jwt-master/src/main/java/com/example/jwt/demo/service/impl/ChatServiceImpl.java
+++ rest-api-jwt-master/src/main/java/com/example/jwt/demo/service/impl/ChatServiceImpl.java
@@ -2,8 +2,11 @@ package com.example.jwt.demo.service.impl;
 
 import com.example.jwt.demo.service.ChatService;
 import org.alicebot.ab.Chat;
+import org.alicebot.ab.History;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ChatServiceImpl implements ChatService {
@@ -11,8 +14,14 @@ public class ChatServiceImpl implements ChatService {
     @Autowired
     private Chat chatSession;
 
+    @Autowired
+    private BmiServiceImpl bmiService;
+
     @Override
     public String getChatResponse(String message) {
+        String response = chatSession.multisentenceRespond(message);
+        // response = " BMI: height:168 age:22 ... "
+        History<String> history = chatSession.inputHistory;
         return chatSession.multisentenceRespond(message);
     }
 }
